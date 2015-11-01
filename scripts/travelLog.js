0bfffd3d8e5b78343d26f2de8aec8e5cc72a9e90
vaetrus <vaetrus@users.noreply.github.com>
2015-11-01 12:32:10 -0500

    initial js

---
+++ scripts/travelLog.js
@@ -0,0 +1,7 @@
+
+L.mapbox.accessToken = 'pk.eyJ1IjoibmdvcXVhbmciLCJhIjoiY2lnZ2l5MGpuODRyNXR5bTZicm5ydjVyYSJ9.i5NluZU88X7Jrz-lYCvomQ';
+L.mapbox.map('map', 'ngoquang.o23bc071', {
+  legendControl: {
+    position: 'bottomright'
+  }
+});
